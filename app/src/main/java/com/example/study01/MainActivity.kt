cc73f731758cdc1a09c80550ae5cc3fef5af87b2
Sang Yup Han <gtd9872@gmail.com>
2020-08-18 04:33:07 +0900

    2주차 숙제까지 완료

--- app/src/main/java/com/example/study01/MainActivity.kt
+++ app/src/main/java/com/example/study01/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.study01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sunnyButton.setOnClickListener {
+            imageView.setImageResource(R.drawable.sunny01)
+            textView.text = "맑은 날"
+        }
+
+        cloudyButton.setOnClickListener {
+            imageView.setImageResource(R.drawable.cloudy01)
+            textView.text = "흐린 날"
+        }
+
+        rainyButton.setOnClickListener {
+            imageView.setImageResource(R.drawable.rainy01)
+            textView.text = "비 오는 날"
+        }
     }
 }
